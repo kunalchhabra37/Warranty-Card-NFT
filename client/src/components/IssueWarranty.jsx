b986ff081e3eb27b61add8d3862bf6c17cfc9739
Chirag-creator17 <chiraggoel172002@gmail.com>
2022-07-19 12:48:23 +0530

    Issue warranty corrections

--- client/src/components/IssueWarranty.jsx
+++ client/src/components/IssueWarranty.jsx
@@ -3,18 +3,27 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 const IssueWarranty = () => {
+
   const [to, setTo] = useState("");
-  const [tokenURI, setTokenURI] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
   const [serialNo, setSerialNo] = useState("");
-  const [warrantyPeriod, setWarrantyPeriod] = useState("");
+  const [product_Id, setProduct_Id] = useState("");
+  const [invoice_no, setInvoice_no] = useState("");
+  const [payment_gateway, setPayment_gateway] = useState("");
+  const [platform, setPlatform] = useState("");
+  const [purchase_date, setPurchase_date] = useState("");
+  const [transaction_id, setTransaction_id] = useState("");
+  const [transaction_method, setTransaction_method] = useState("");
+  const [warranty_period, setWarranty_period] = useState("");
+  const [attributes, setAttributes] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    console.log(to, tokenURI, serialNo, warrantyPeriod);
+    console.log(to, name, description, serialNo, product_Id, invoice_no, payment_gateway, platform, purchase_date, transaction_id, transaction_method, warranty_period, attributes);
   };
   return (
-    <div className="container1">
+    <div className="container1 mt-3">
       <h1>Issue Warranty</h1>
       <Form>
         <Form.Group className="mb-3 ctrl">
@@ -27,34 +36,115 @@ const IssueWarranty = () => {
           />
         </Form.Group>
         <Form.Group className="mb-3 ctrl">
-          <Form.Label>TokenURI</Form.Label>
+          <Form.Label>Name</Form.Label>
           <Form.Control
             type="text"
-            placeholder="TokenURI"
-            value={tokenURI}
-            onChange={(e) => setTokenURI(e.target.value)}
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
         </Form.Group>
         <Form.Group className="mb-3 ctrl">
-          <Form.Label>serialNo</Form.Label>
+          <Form.Label>Description</Form.Label>
           <Form.Control
             type="text"
-            placeholder="SerialNo"
+            placeholder="Enter Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3 ctrl">
+          <Form.Label>Serial No</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter serialNo"
             value={serialNo}
             onChange={(e) => setSerialNo(e.target.value)}
           />
         </Form.Group>
         <Form.Group className="mb-3 ctrl">
-          <Form.Label>Warranty End</Form.Label>
+          <Form.Label>Product Id</Form.Label>
           <Form.Control
             type="text"
-            pattern="^[0-9]{1,}$"
-            title="Only numbers are allowed"
-            placeholder="Warranty End"
-            value={warrantyPeriod}
-            onChange={(e) => setWarrantyPeriod(e.target.value)}
+            placeholder="Enter product_Id"
+            value={product_Id}
+            onChange={(e) => setProduct_Id(e.target.value)}
           />
         </Form.Group>
+        <Form.Group className="mb-3 ctrl">
+          <Form.Label>Invoice No</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter invoice_no"
+            value={invoice_no}
+            onChange={(e) => setInvoice_no(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3 ctrl">
+          <Form.Label>Payment Gateway</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter payment_gateway"
+            value={payment_gateway}
+            onChange={(e) => setPayment_gateway(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3 ctrl">
+          <Form.Label>Platform</Form.Label> 
+          <Form.Control
+            type="text"
+            placeholder="Enter platform"
+            value={platform}
+            onChange={(e) => setPlatform(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3 ctrl">
+          <Form.Label>Purchase Date</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter purchase_date"
+            value={purchase_date}
+            onChange={(e) => setPurchase_date(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3 ctrl">
+          <Form.Label>Transaction Id</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter transaction_id"
+            value={transaction_id}
+            onChange={(e) => setTransaction_id(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3 ctrl">
+          <Form.Label>Transaction Method</Form.Label> 
+          <Form.Control
+            type="text"
+            placeholder="Enter transaction_method"
+            value={transaction_method}
+            onChange={(e) => setTransaction_method(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3 ctrl">
+          <Form.Label>Warranty Period</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter warranty_period"
+            value={warranty_period}
+            onChange={(e) => setWarranty_period(e.target.value)}
+          />  
+        </Form.Group>
+        <Form.Group className="mb-3 ctrl">
+          <Form.Label>Attributes</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter attributes"
+            value={attributes}
+            onChange={(e) => setAttributes(e.target.value)}
+          />
+        </Form.Group>
+
+          
         <Button variant="primary" type="submit" onClick={handleSubmit}>
           Issue
         </Button>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,26 +1,28 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Button from './Button';
 function NavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
       <Container>
         <Navbar.Brand href="/">Warranty Card</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav>
-            <Nav.Link href="/issue-warranty">Issue Warranty</Nav.Link>
+            {/* all  */}
             <Nav.Link href="/check-authenticity">Check Authenticity</Nav.Link>
+            <Nav.Link href="/check-expiry">Check Expiry</Nav.Link>
+            <Nav.Link href="/get-warranty-card">Get Warranty Card</Nav.Link>
             <Nav.Link href="/approve">Approve</Nav.Link>
+            <Nav.Link href="/transfer">Transfer</Nav.Link>
+            {/* minter only */}
+            <Nav.Link href="/issue-warranty">Issue Warranty</Nav.Link>
+            {/* minter admin only */}
             <Nav.Link href="/grant-role">Grant Role</Nav.Link>
             <Nav.Link href="/revoke-role">Revoke Role</Nav.Link>
-            <Nav.Link href="/check-expiry">Check Expiry</Nav.Link>
-            <Nav.Link href="/transfer">Transfer</Nav.Link>
-            <Nav.Link href="/get-warranty-card">Get Warranty Card</Nav.Link>
           </Nav>
-            <Button text="Conntect to Wallet" className="btn-primary text-white"/>
+          <Button text="Conntect to Wallet" className="btn-primary text-white btn-style"/>
         </Navbar.Collapse>
       </Container>
     </Navbar>

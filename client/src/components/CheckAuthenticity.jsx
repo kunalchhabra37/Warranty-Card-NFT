6cfa394558318e9357cb101fd0cabb3fa80d787e
Chirag-creator17 <chiraggoel172002@gmail.com>
2022-07-25 10:59:22 +0530

    added loader

--- client/src/components/CheckAuthenticity.jsx
+++ client/src/components/CheckAuthenticity.jsx
@@ -4,17 +4,47 @@ import Form from "react-bootstrap/Form";
 import { WarrantyCardContext } from "../context/WarrantyCardContext";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { validateBigInt,validateAddress } from "./Validation";
+import Loader from "react-loader";
+import { Bars ,ThreeDots} from  'react-loader-spinner'
+
+import { validateBigInt, validateAddress } from "./Validation";
 const CheckAuthenticity = () => {
   const { checkAuthenticity } = useContext(WarrantyCardContext);
-  const [to, setTo] = useState ("");
+  const [to, setTo] = useState("");
   const [tokenID, settokenID] = useState("");
   const [serialNo, setSerialNo] = useState("");
-  const [res, setRes] = useState(false)
+  const [res, setRes] = useState(false);
+  const [loading, setLoading] = useState(false);
   const handleSubmit = async (e) => {
     e.preventDefault();
-      if (!validateAddress(to)) {
-        toast.warning("Not a valid address", {
+    setLoading(true);
+    if (!validateAddress(to)) {
+      toast.warning("Not a valid address", {
+        position: "top-right",
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      setLoading(false);
+    } else if (!validateBigInt(tokenID)) {
+      toast.warning("Not a valid tokenID", {
+        position: "top-right",
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      setLoading(false);
+    } else {
+      let res = await checkAuthenticity(to, tokenID, serialNo);
+      if (res.hasOwnProperty("error")) {
+        console.log(res.error);
+        toast.warning(res.error, {
           position: "top-right",
           autoClose: 4000,
           hideProgressBar: false,
@@ -23,79 +53,66 @@ const CheckAuthenticity = () => {
           draggable: true,
           progress: undefined,
         });
+      } else {
+        console.log(res);
+        setRes(res);
+        setTo("");
+        setSerialNo("");
+        settokenID("");
       }
-      else if (!validateBigInt(tokenID)) {
-        toast.warning("Not a valid tokenID", {
-          position: "top-right",
-          autoClose: 4000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        });
-      }
-      else {
-        let res = await checkAuthenticity(to, tokenID, serialNo);
-        if (res.hasOwnProperty('error')) {
-          console.log(res.error);
-          toast.warning(res.error, {
-            position: "top-right",
-            autoClose: 4000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-          });
-        } else {
-          console.log(res)
-          setRes(res)
-          setTo('')
-          setSerialNo('')
-          settokenID('')
-        }
-      }
+      setLoading(false);
+    }
   };
   return (
     <>
-    <div className="container1">
-      <h1>Check Authenticity</h1>
-      <Form>
-        <Form.Group className="mb-3 ctrl">
-          <Form.Label>To</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="To"
-            value={to}
-            onChange={(e) => setTo(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3 ctrl">
-          <Form.Label>tokenID</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="tokenID"
-            value={tokenID}
-            onChange={(e) => settokenID(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3 ctrl">
-          <Form.Label>SerialNo</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="SerialNo"
-            value={serialNo}
-            onChange={(e) => setSerialNo(e.target.value)}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit" onClick={handleSubmit}>
-          Check
-        </Button>
-      </Form>
-    </div>
-    {res && <p className="text-white"> {res} </p>}
+      <div className="container1">
+        <h1>Check Authenticity</h1>
+        <Form>
+          <Form.Group className="mb-3 ctrl">
+            <Form.Label>To</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="To"
+              value={to}
+              onChange={(e) => setTo(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3 ctrl">
+            <Form.Label>tokenID</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="tokenID"
+              value={tokenID}
+              onChange={(e) => settokenID(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3 ctrl">
+            <Form.Label>SerialNo</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="SerialNo"
+              value={serialNo}
+              onChange={(e) => setSerialNo(e.target.value)}
+            />
+          </Form.Group>
+          {loading ? (
+            <div className="text-center">
+              <Bars 
+                color="#00BFFF"
+                width={273}
+              wrapperStyle
+              wrapperClass />
+            </div>
+            
+          ) : (
+            <Button variant="primary" type="submit" onClick={handleSubmit}>
+              Check
+            </Button>
+          )}
+        </Form>
+      </div>
+      {res && <p className="text-white"> {res} </p>}
     </>
   );
 };
-export default CheckAuthenticity
+export default CheckAuthenticity;
